8420e228bd93d5db4beb9fedc0c095efc3947896
ilyas48 <ilyas48@yandex.ru>
2023-04-24 12:28:19 +0300

    New shop_electronics

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item1():
+    return Item("Смартфон", 1000, 2)
+
+
+def test_calculate_total_price(item1):
+    assert item1.calculate_total_price() == 2000
+
+
+def test_apply_discount(item1):
+    Item.pay_rate = 0.85
+    assert item1.apply_discount() == 850
+
+
